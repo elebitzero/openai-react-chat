09dbb36672bdf90af7758b723f230fa947ad5cb6
elebitzero <dave.lefaive@gmail.com>
2023-07-24 19:34:16 -0700

    Removed gradient from main scrollbar.

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ const App = () => {
                 </div>
                 <Chat chatBlocks={messageBlocks}/>
                 <div
-                    className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent md:bg-vert-light-gradient bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
+                    className="absolute bottom-0 left-0 w-full border-t md:border-t-0 dark:border-white/20 md:border-transparent md:dark:border-transparent bg-white dark:bg-gray-800 md:!bg-transparent dark:md:bg-vert-dark-gradient pt-2">
                     <form onSubmit={handleSubmit}
                           className="stretch mx-2 flex flex-row gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
                         <div className="relative flex h-full flex-1 md:flex-col">
