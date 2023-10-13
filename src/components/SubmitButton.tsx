3d4e5102d5a9b5955c6b133f1199855f3029032b
elebitzero <dave.lefaive@gmail.com>
2023-10-13 14:23:40 -0700

    Reformat/cleanup.

--- src/components/SubmitButton.tsx
+++ src/components/SubmitButton.tsx
@@ -15,18 +15,19 @@ export const SubmitButton: React.FC<SubmitButtonProps> = ({loading, disabled, st
 
     return (
         <Tooltip title="Send message" side="top">
-        <button
-            type="submit"
-            disabled={loading || disabled}
-            className="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-2 md:right-3 disabled:opacity-40"
-            style={style}
-        >
-            {loading ? (
-                <EllipsisHorizontalIcon className="animate-ellipsis-pulse" width={24} height={24} stroke={strokeColor}/>
-            ) : (
-                <PaperAirplaneIcon width={24} height={24} stroke={strokeColor}/>
-            )}
-        </button>
+            <button
+                type="submit"
+                disabled={loading || disabled}
+                className="absolute p-1 rounded-md bottom-1.5 md:bottom-2.5 hover:bg-gray-100 enabled:dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent right-2 md:right-3 disabled:opacity-40"
+                style={style}
+            >
+                {loading ? (
+                    <EllipsisHorizontalIcon className="animate-ellipsis-pulse" width={24} height={24}
+                                            stroke={strokeColor}/>
+                ) : (
+                    <PaperAirplaneIcon width={24} height={24} stroke={strokeColor}/>
+                )}
+            </button>
         </Tooltip>
     );
 };
