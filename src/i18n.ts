a29acb5ab66cb379d8fd0139c5ce18ab49fed323
elebitzero <dave.lefaive@gmail.com>
2024-01-10 18:08:51 -0800

    Added language detection for the browser language.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,12 +1,17 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(Backend)
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     fallbackLng: 'en',
+    detection: {
+      order: ['navigator', 'querystring', 'cookie', 'localStorage', 'htmlTag', 'path'],
+    },
     //debug: true,
     interpolation: {
       escapeValue: false,
