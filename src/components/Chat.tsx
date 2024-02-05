9559e35ae478976eeb17598b6796493c3ad31900
elebitzero <dave.lefaive@gmail.com>
2024-02-04 17:22:07 -0800

    Add knowledge cutoff date data to model details.

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,6 +7,7 @@ import {OPENAI_MODEL_LIST} from "../config";
 import {toast} from "react-toastify";
 import {ChatMessage} from "../models/ChatCompletion";
 import {useTranslation} from 'react-i18next';
+import Tooltip from "./Tooltip";
 
 interface Props {
     chatBlocks: ChatMessage[];
@@ -25,28 +26,28 @@ const Chat: React.FC<Props> = ({chatBlocks, onChatScroll, allowAutoScroll}) => {
 
         if (OPENAI_MODEL_LIST && OPENAI_MODEL_LIST.length > 0) {
             setModels(OPENAI_MODEL_LIST.map(id => {
-                    return {
-                        id: id,
-                        object: 'model',
-                        context_window: 0,
-                        knowledge_cutoff: '',
-                        owned_by: 'not-set',
-                        permission: []
-                    } as OpenAIModel;
-                })
+                  return {
+                      id: id,
+                      object: 'model',
+                      context_window: 0,
+                      knowledge_cutoff: '',
+                      owned_by: 'not-set',
+                      permission: []
+                  } as OpenAIModel;
+              })
             );
         } else {
             ChatService.fetchModels()
-                .then(fetchedModels => {
-                    setModels(fetchedModels);
-                })
-                .catch(err => {
-                    if (err && err.message) {
-                        setError(err.message);
-                    } else {
-                        setError('Error fetching model list');
-                    }
-                });
+              .then(fetchedModels => {
+                  setModels(fetchedModels);
+              })
+              .catch(err => {
+                  if (err && err.message) {
+                      setError(err.message);
+                  } else {
+                      setError('Error fetching model list');
+                  }
+              });
         }
     }, []);
 
@@ -74,21 +75,32 @@ const Chat: React.FC<Props> = ({chatBlocks, onChatScroll, allowAutoScroll}) => {
         const chatContainer = chatDivRef.current;
         if (chatContainer) {
             const isAtBottom =
-                chatContainer.scrollHeight - chatContainer.scrollTop ===
-                chatContainer.clientHeight;
+              chatContainer.scrollHeight - chatContainer.scrollTop ===
+              chatContainer.clientHeight;
 
             // Initially hide the button if chat is at the bottom
             onChatScroll(isAtBottom);
         }
     }, []);
 
+    const findModelById = (id: string): OpenAIModel | undefined => {
+        return models.find(model => model.id === id);
+    };
+
+    const formatContextWindow = (context_window: number | undefined) => {
+        if (context_window) {
+            return Math.round(context_window / 1000) + 'k';
+        }
+        return '?k';
+    }
+
     const handleScroll = () => {
         if (chatDivRef.current) {
             const scrollThreshold = 20;
             const isAtBottom =
-                chatDivRef.current.scrollHeight -
-                chatDivRef.current.scrollTop <=
-                chatDivRef.current.clientHeight + scrollThreshold;
+              chatDivRef.current.scrollHeight -
+              chatDivRef.current.scrollTop <=
+              chatDivRef.current.clientHeight + scrollThreshold;
 
             // Notify parent component about the auto-scroll status
             onChatScroll(isAtBottom);
@@ -101,26 +113,44 @@ const Chat: React.FC<Props> = ({chatBlocks, onChatScroll, allowAutoScroll}) => {
     };
 
     return (
+      <div className="flex-1 overflow-auto" ref={chatDivRef} id={'chat-container'} onScroll={handleScroll}>
+          <div className="flex flex-col items-center text-sm dark:bg-gray-800">
+              <div
+                className={`flex w-full items-center justify-center gap-1 p-3 text-gray-500 dark:border-gray-900/50 dark:bg-gray-700 dark:text-gray-300 ${!isNewConversation ? 'border-b border-black/10' : ''}`}>
+                  <div className="flex items-center flex-row gap-1">
+                        <span
+                          style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>{t('model')}
+                            {isNewConversation ? '' : (
+                              <span>
+                                  <span style={{marginLeft:'0.25em'}}>{ChatService.getSelectedModelId()}</span>
+                                  <Tooltip title={t('context-window')} side="bottom" sideOffset={10}>
+                                      <span style={{marginLeft: '10px', fontSize: '0.85rem', color: '#6b7280'}}>
+                                        {formatContextWindow(findModelById(ChatService.getSelectedModelId())?.context_window)}
+                                      </span>
+                                  </Tooltip>
+                                     <Tooltip title={t('knowledge-cutoff')} side="bottom" sideOffset={10}>
+                                      <span style={{marginLeft: '10px', fontSize: '0.85rem', color: '#6b7280'}}>
+                                        {findModelById(ChatService.getSelectedModelId())?.knowledge_cutoff}
+                                      </span>
+                                  </Tooltip>
+                              </span>
+                            )
+                            }
+                        </span>
 
-        <div className="flex-1 overflow-auto" ref={chatDivRef} id={'chat-container'} onScroll={handleScroll}>
-            <div className="flex flex-col items-center text-sm dark:bg-gray-800">
-                <div
-                    className={`flex w-full items-center justify-center gap-1 p-3 text-gray-500 dark:border-gray-900/50 dark:bg-gray-700 dark:text-gray-300 ${!isNewConversation ? 'border-b border-black/10' : ''}`}>
-                    <div className="flex items-center flex-row gap-1">
-                        <span>{t('model')} {isNewConversation ? '' : ChatService.getSelectedModelId()}</span>
-                        <span className="flex-grow">
+                      <span className="flex-grow">
                           <div style={{display: isNewConversation ? 'block' : 'none', width: '50ch'}}>
                             <ModelSelect models={models}/>
                           </div>
                       </span>
-                    </div>
-                </div>
-                {chatBlocks.map((block) => (
-                    <ChatBlock key={`chat-block-${block.id}`} block={block}/>
-                ))}
-                <div className="w-full h-24 flex-shrink-0"></div>
-            </div>
-        </div>
+                  </div>
+              </div>
+              {chatBlocks.map((block) => (
+                <ChatBlock key={`chat-block-${block.id}`} block={block}/>
+              ))}
+              <div className="w-full h-24 flex-shrink-0"></div>
+          </div>
+      </div>
     );
 };
 
