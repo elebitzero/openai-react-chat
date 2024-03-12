038258404e7a6efffcb94fc298fc21045d1be17d
elebitzero <dave.lefaive@gmail.com>
2024-03-12 11:24:04 -0600

    Added light background for custom chat icons in sidebar.

--- src/components/ChatShortcuts.tsx
+++ src/components/ChatShortcuts.tsx
@@ -31,7 +31,7 @@ const ChatShortcuts: React.FC = () => {
       {chatSettings.map((setting) => (
         <Link to={`/g/${setting.id}`} key={setting.id}
               className="flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all hover:bg-gray-100 dark:hover:bg-gray-800">
-          {setting.icon?.data && <img src={setting.icon.data} alt="" style={{width: 24, height: 24}}/>}
+          {setting.icon?.data && <img src={setting.icon.data} alt="" className="bg-gray-100 dark:bg-gray-400" style={{width: 24, height: 24}}/>}
           <span
             className="flex-1 overflow-hidden whitespace-nowrap overflow-ellipsis max-h-5 break-all relative dark:text-gray-100 text-gray-800">
       {setting.name}
