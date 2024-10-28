52f9b6cb26b738c5722f810c4c87767ca36f60e9
elebitzero <dave.lefaive@gmail.com>
2024-10-28 00:04:16 -0600

    Make copy button float over code while scrolling.

--- src/components/MarkdownBlock.tsx
+++ src/components/MarkdownBlock.tsx
@@ -62,43 +62,64 @@ const MarkdownBlock: React.FC<ChatBlockProps> = ({markdown, role, loading}) => {
     return isInline ? (
         inlineCodeBlock({value: value, language})
     ) : (
-        <div className="border border-gray-200 dark:border-gray-800 rounded-md codeBlockContainer dark:bg-gray-850">
-          <div
-              className="flex items-center relative text-gray-900 dark:text-gray-200 bg-gray-200 dark:bg-gray-850 px-4 py-1.5 text-xs font-sans justify-between rounded-t-md">
-            <span>{language}</span>
-            <CopyButton text={children}/>
-          </div>
-          <div className="overflow-y-auto">
-            <SyntaxHighlighter
-                language={language}
-                style={userSettings.theme === 'dark' ? coldarkDark : oneLight}
-                customStyle={{margin: '0'}}
-            >
-              {value}
-            </SyntaxHighlighter>
-            {/* <code {...props} className={className}>
-                        {children}
-                    </code>*/}
+      <div className="relative border border-gray-200 dark:border-gray-800 rounded-md codeBlockContainer dark:bg-gray-850">
+        <div
+          className="flex items-center text-gray-900 dark:text-gray-200 bg-gray-200 dark:bg-gray-850 px-4 py-2 text-xs font-sans justify-between rounded-t-md">
+          <span>{language}</span>
+        </div>
+        <div className="sticky top-9 md:top-[5.75rem]">
+          <div className="absolute bottom-0 right-2 flex h-8 items-center">
+            <CopyButton className="rounded bg-gray-200 dark:bg-gray-850" text={children}/>
           </div>
         </div>
+        <div className="overflow-y-auto">
+          <SyntaxHighlighter
+            language={language}
+            style={userSettings.theme === 'dark' ? coldarkDark : oneLight}
+            customStyle={
+            {
+              margin: '0'
+            }
+          }
+          >
+            {value}
+          </SyntaxHighlighter>
+          {/* <code {...props} className={className}>
+                        {children}
+                    </code>*/}
+        </div>
+      </div>
+    );
+  }
+
+  function customPre({children, className, ...props}: any) {
+    return (
+      <pre
+        className={`custom-pre-block ${className || ''}`}
+        style={{ overflow: 'visible' }}
+        {...props}
+      >
+      {children}
+    </pre>
     );
   }
 
   const renderers = {
     code: codeBlock,
+    pre: customPre,
   };
 
   return (
-      <div>
-        <ReactMarkdown
-            remarkPlugins={[gfm, remarkMath]}
-            components={renderers}
-            rehypePlugins={[rehypeKatex, rehypeInlineCodeProperty]}
-        >
-          {markdown}
-        </ReactMarkdown>
-        {loading && <span className="streaming-dot">•••</span>}
-      </div>
+    <div>
+      <ReactMarkdown
+        remarkPlugins={[gfm, remarkMath]}
+        components={renderers}
+        rehypePlugins={[rehypeKatex, rehypeInlineCodeProperty]}
+      >
+        {markdown}
+      </ReactMarkdown>
+      {loading && <span className="streaming-dot">•••</span>}
+    </div>
   );
 };
 
